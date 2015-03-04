8b104ae77feb462e15aeb795dba912a0b7aeab8a
qingyun <tangqingyuncx@qq.com>
2015-03-04 11:51:07 +0800

    add 360doc.tool

---
+++ 360DOC.Tool/360DOC.Tool/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _360DOC.Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
